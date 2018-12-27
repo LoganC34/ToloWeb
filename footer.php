6afe2f03bf821da90d4d97a9832c29f10262f5cc
LoganC34 <Logan@bramblettgrp.com>
2018-12-27 16:57:57 -0600

    test

--- footer.php
+++ footer.php
@@ -1,3 +1,40 @@
 
+
+<footer class="footer">
+    <div class="footer__content">
+        <div class="footer__element">
+            <div class="footer__logo">
+                <a href="#" class="footer__logo-link">
+                    <img class="footer__logo-source" src="SVG/logo-01.svg">
+                </a>
+            </div>
+            <div class="footer__social">
+                <a href="#" class="footer__social-link">
+                    <img class="footer__social-link-source" src="SVG/instagram.svg">
+                </a>
+            </div>
+        </div>
+        <div class="footer__element">
+            <form class="contact-form">
+                <div class="input-group">
+                    <label class="input-group__label">Name:</label>
+                    <input class="input-group__input">
+                </div>
+                <div class="input-group">
+                    <label class="input-group__label">Email:</label>
+                    <input class="input-group__input">
+                </div>
+                <div class="input-group">
+                    <label class="input-group__label">Message</label>
+                    <textarea class="input-group__input"></textarea>
+                </div>
+                <div class="input-group">
+                    <input class="input-group__input input-group__input--btn" type="submit">
+                </div>
+            </form>
+        </div>
+    </div>
+</footer>
+
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -1,4 +1,10 @@
 <?php include 'header.php' ?>
 
+<section class="section">
+    <h2 class="section__title">Welcome to Tolo!</h2>
+    <div class="section__content">
+        <p>Dolore adipisicing aliqua aliqua dolore. Esse voluptate id nisi est qui reprehenderit excepteur labore laboris. Quis tempor sunt id ex cupidatat do aliqua duis. Aute amet velit mollit proident excepteur adipisicing ex elit eiusmod tempor dolor.</p>
+    </div>
+</section>
 
 <?php include 'footer.php' ?>
